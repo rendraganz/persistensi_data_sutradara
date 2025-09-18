ab90c4403d3b6be30582c00ea10902672895abd8
rendraganz <rendrairawan170.com>
2025-09-18 22:32:04 +0700

    update CRUD

--- database.js
+++ database.js
@@ -8,7 +8,7 @@ const db = new sqlite3.Database(DBSOURCE, (err) => {
         throw err;
     } else {
         console.log('Connected to the SQLite database.');
-
+        // movies
         db.run(`CREATE TABLE IF NOT EXISTS movies (
             id INT PRIMARY KEY,
             title TEXT NOT NULL,
@@ -23,8 +23,23 @@ const db = new sqlite3.Database(DBSOURCE, (err) => {
                 db.run(insert, [2, "Tukang Bubur Naik Haji", "Tukang Bubur", 2014]);
                 console.log("Table 'movies' already exists.");
             }
-        }
-        )
+        });
+
+        // directors
+        db.run(`CREATE TABLE IF NOT EXISTS directors (
+            id INTEGER PRIMARY,
+            name TEXT NOT NULL,
+            birthYear INT
+        );`, (err) => {
+            if (err) {
+                console.error("Error creating table 'directors':", err.message);
+            } else {
+                const insert = 'INSERT INTO directors (id, name, birthYear) VALUES (?,?,?)';
+                db.run(insert, [1, "Rendra", 2036]);
+                db.run(insert, [2, "Tukang Bubur", 2014]);
+                console.log("Table 'directors' already exists.");
+            }
+        });
     }
 });
 module.exports = db;
--- server.js
+++ server.js
@@ -48,6 +48,92 @@ app.get('/movies', (req, res) => {
     });
 });
 
+// get movie dari id
+app.get('/movies/:id', (req, res) => {
+    const sql = "SELECT * FROM movies WHERE id = ?";
+    db.get(sql, [req.params.id], (err, row) => {
+        if (err) return res.status(400).json({ error: err.message });
+        res.json(row);
+    });
+});
+
+// tambah movie
+app.post('/movies', (req, res) => {
+    const { title, director, year } = req.body;
+    const sql = "INSERT INTO movies (title, director, year) VALUES (?,?,?)";
+    db.run(sql, [title, director, year], function(err) {
+        if (err) return res.status(400).json({ error: err.message });
+        res.json({ id: this.lastID, title, director, year });
+    });
+});
+
+// update movie by id
+app.put('/movies/:id', (req, res) => {
+    const { title, director, year } = req.body;
+    const sql = "UPDATE movies SET title = ?, director = ?, year = ? WHERE id = ?";
+    db.run(sql, [title, director, year, req.params.id], function(err) {
+        if (err) return res.status(400).json({ error: err.message });
+        res.json({ updatedID: req.params.id, title, director, year });
+    });
+});
+
+// hapus movie
+app.delete('/movies/:id', (req, res) => {
+    const sql = "DELETE FROM movies WHERE id = ?";
+    db.run(sql, [req.params.id], function(err) {
+        if (err) return res.status(400).json({ error: err.message });
+        res.json({ deletedID: req.params.id });
+    });
+});
+
+// menampilkan semua director
+app.get('/directors', (req, res) => {
+    const sql = "SELECT * FROM directors ORDER BY id ASC;";
+    db.all(sql, [], (err, rows) => {
+        if (err) return res.status(400).json({ error: err.message });
+        res.json(rows);
+    });
+});
+
+// get director dari id
+app.get('/directors/:id', (req, res) => {
+    const sql = "SELECT * FROM directors WHERE id = ?";
+    db.get(sql, [req.params.id], (err, row) => {
+        if (err) return res.status(400).json({ error: err.message });
+        res.json(row);
+    });
+});
+
+// menambah director
+app.post('/directors', (req, res) => {
+    const { name, birthYear } = req.body;
+    const sql = "INSERT INTO directors (name, birthYear) VALUES (?,?)";
+    db.run(sql, [name, birthYear], function(err) {
+        if (err) return res.status(400).json({ error: err.message });
+        res.json({ id: this.lastID, name, birthYear });
+    });
+});
+
+
+// update director 
+app.put('/directors/:id', (req, res) => {
+    const { name, birthYear } = req.body;
+    const sql = "UPDATE directors SET name = ?, birthYear = ? WHERE id = ?";
+    db.run(sql, [name, birthYear, req.params.id], function(err) {
+        if (err) return res.status(400).json({ error: err.message });
+        res.json({ updatedID: req.params.id, name, birthYear });
+    });
+});
+
+// hapus id
+app.delete('/directors/:id', (req, res) => {
+    const sql = "DELETE FROM directors WHERE id = ?";
+    db.run(sql, [req.params.id], function(err) {
+        if (err) return res.status(400).json({ error: err.message });
+        res.json({ deletedID: req.params.id });
+    });
+});
+
 //handle 404
 app.use((req, res) => {
     res.status(404).json({ error: "Route not found" });
